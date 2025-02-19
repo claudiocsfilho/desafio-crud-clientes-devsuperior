ffae5756d6317394d01834732c5dee99b2a7528e
Claudio Filho <claudiocsfilho@gmail.com>
2025-02-19 14:38:06 -0300

    First commit

---
+++ src/main/java/com/claudiocsfilho/desafio3_crud_clientes/Desafio3CrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.claudiocsfilho.desafio3_crud_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio3CrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio3CrudClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claudiocsfilho/desafio3_crud_clientes/Desafio3CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudiocsfilho.desafio3_crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
